ebf9fef49101098b5a2c7a9875bdf0165a4ab876
Juan <jm-barreto1@hotmail.com>
2022-04-11 23:41:22 -0400

    put functions into classes, created startup class, added Https requests

--- Beacon.py
+++ Beacon.py
@@ -1,60 +1,69 @@
-
 import sys
-import socket
+import requests
 import json
-# from bluepy.btle import Scanner, DefaultDelegate
-#
-# class myDelegate(DefaultDelegate):
-#     def __init__(self):
-#             DefaultDelegate.__init__(self)
-#     def handleDiscovery(self, dev, isNewDev, isNewData):
-#         if isNewDev and dev.addr == "dd:33:16:00:02:dc":
-#                 print(f"my name is {dev.getValueText(9)}")
-#                 beacondict = {"address":dev.addr,"rssi":dev.rssi}
-#     def handleNotification(self, cHandle, data):
-#
-# #Initialize Beacon
-# p = btle.Peripheral("dd:33:16:00:02:dc")
-# p.setDelegate(myDelegate())
-#
+from datetime import datetime
+
+from bluepy.btle import Scanner, DefaultDelegate, Peripheral
+
+
+# this handles beacon discovery and puts it into a dict
+class myDelegate(DefaultDelegate):
+    def __init__(self):
+        DefaultDelegate.__init__(self)
+
+    def handleDiscovery(self, dev, isNewDev, isNewData):
+        if isNewDev and dev.addr == "dd:33:16:00:02:dc":
+            now = datetime.now()
+            now_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            print(f"my name is {dev.getValueText(9)}")
+
+            self.beacondict = {
+                "sniffer_serial": "xxxx-xxxx-xxxx-xxxx",
+                "address": dev.addr,
+                "rssi": dev.rssi,
+                "source_addr": "192.168.4.7",
+                "time": now_string
+            }
+            return True
+
+
+# bdict = myDelegate().beacondict
 # #setup notifications
-# service = p.getServicebyUUID()
-# ch = service.getCharacteristics(char_uuid)[0]
-# c.write(setup_data)
+#
+#
+#
+#
+#
+#
 
-#
-#
-#
-#
-#
-#
-host = "192.168.1.12"
-port = 9988
-bdict = {"address": "address", "rssi":"rssi"}
+class Data:
+    def __init__(self):
+        Data.__init__(self)
+
+    def connection(self, update_data):
+        # send dictionary with update data here
+
+        req = requests.put('http://192.168.4.1/sniffer/event/',
+                           headers={'content-type': 'application/json'},
+                           json=update_data,
+                           )
+        #400 is if the code sent is not a json file or not able to be sent in a json packet
+        if req.status_code == 400:
+            print("the data is incorrectly formatted or sent incorrectly")
+            print(req.text)
+        else:
+            print("there was an error or something")
+
+        # There was an error processing the request
+
+        req.close()
 
 
-def connection():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("socket created")
-    s.connect((host,port))
+class Startup:
+    # Initialize Beacon
+    r = Data
+    p = btle.Peripheral("dd:33:16:00:02:dc")
     while True:
-        try:
-            command = input("Enter:")
-            if command == 'kill':
-                    s.send(str.encode(command))
-                    break
-            elif command == 'dict':
-                data = str.encode(json.dumps(bdict))
-                s.sendall(data)
-            s.send(str.encode(command))
-
-            print("\nAwaiting reply from: " + host)
-            reply = s.recv(1024)
-            print(host, " : ", reply)
-            reply = reply.decode('utf-8')
-            print(reply)
-        except socket.error() as msg:
-            print(msg)
-    s.close()
-
-connection()
+        p.setDelegate(myDelegate())
+        if (p):
+            r.connection(myDelegate().beacondict)
--- README.md
+++
@@ -1,1 +0,0 @@
-# SnifferSlave
